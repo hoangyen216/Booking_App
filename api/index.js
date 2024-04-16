353a4a818bffa71148de755aa0bf2b3732ecfff8
minhduc65t <132641313+minhduc65t@users.noreply.github.com>
2024-04-16 21:29:47 +0700

    chuc nang hien thi danh sach place

--- api/index.js
+++ api/index.js
@@ -54,3 +54,19 @@ app.post('/places', (req, res) => {
         res.json(placeDoc);
     });
 });
+
+app.get('/user-places', (req, res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+        const { id } = userData;
+        res.json(await Place.find({ owner: id }));
+
+    });
+});
+
+app.get('/places', async (req, res) => {
+    res.json(await Place.find());
+});
+
+app.listen(4000);
+
